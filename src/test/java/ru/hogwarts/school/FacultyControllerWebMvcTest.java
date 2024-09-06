6bd2c80b2a76a3fb3084e41db910a6b5caf10704
agorohov <aleksandr.gorohov@gmail.com>
2024-09-06 17:30:57 +0300

    приступил к FacultyControllerWebMvcTest

--- src/test/java/ru/hogwarts/school/FacultyControllerWebMvcTest.java
+++ src/test/java/ru/hogwarts/school/FacultyControllerWebMvcTest.java
@@ -1,5 +1,48 @@
 package ru.hogwarts.school;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.hogwarts.school.controller.FacultyController;
+import ru.hogwarts.school.repository.AvatarRepository;
+import ru.hogwarts.school.repository.FacultyRepository;
+import ru.hogwarts.school.repository.StudentRepository;
+import ru.hogwarts.school.service.AvatarService;
+import ru.hogwarts.school.service.FacultyService;
+import ru.hogwarts.school.service.StudentService;
+
+@WebMvcTest
 class FacultyControllerWebMvcTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private FacultyRepository facultyRepository;
+    @MockBean
+    private StudentRepository studentRepository;
+    @MockBean
+    private AvatarRepository avatarRepository;
+
+    @SpyBean
+    private FacultyService facultyService;
+    @SpyBean
+    private StudentService studentService;
+    @SpyBean
+    private AvatarService avatarService;
+
+    @InjectMocks
+    private FacultyController facultyController;
+
+    @Test
+    void contextLoads() {
+        Assertions.assertThat(facultyController).isNotNull();
+    }
+
 
 }
