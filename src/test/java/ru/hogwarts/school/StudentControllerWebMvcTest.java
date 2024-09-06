ac0ad97f6d0196efb89b3d900a00dc7ca522b4a0
agorohov <aleksandr.gorohov@gmail.com>
2024-09-06 10:51:50 +0300

    создал createTestData, createWithRandomFacultyTest, createTest, getTest, editTest

--- src/test/java/ru/hogwarts/school/StudentControllerWebMvcTest.java
+++ src/test/java/ru/hogwarts/school/StudentControllerWebMvcTest.java
@@ -1,5 +1,173 @@
 package ru.hogwarts.school;
 
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.hogwarts.school.controller.StudentController;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.AvatarRepository;
+import ru.hogwarts.school.repository.FacultyRepository;
+import ru.hogwarts.school.repository.StudentRepository;
+import ru.hogwarts.school.service.AvatarService;
+import ru.hogwarts.school.service.FacultyService;
+import ru.hogwarts.school.service.StudentService;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
 class StudentControllerWebMvcTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private StudentRepository studentRepository;
+
+    @MockBean
+    private FacultyRepository facultyRepository;
+
+    @MockBean
+    private AvatarRepository avatarRepository;
+
+    @SpyBean
+    private StudentService service;
+
+    @SpyBean
+    private FacultyService facultyService;
+
+    @SpyBean
+    private AvatarService avatarService;
+
+    @InjectMocks
+    private StudentController studentController;
+
+    private Faculty faculty;
+    private Student student;
+
+    @BeforeEach
+    void createTestData() throws Exception {
+        long id = 1L;
+        String name = "Gena";
+        int age = 34;
+        String facultyName = "Grif";
+        String facultyColor = "red";
+
+        faculty = new Faculty();
+        faculty.setId(1L);
+        faculty.setName(facultyName);
+        faculty.setColor(facultyColor);
+
+        student = new Student();
+        student.setId(id);
+        student.setName(name);
+        student.setAge(age);
+        student.setFaculty(faculty);
+    }
+
+    @Test
+    void createWithRandomFacultyTest() throws Exception {
+        when(facultyRepository.findRandom()).thenReturn(Optional.of(faculty));
+        when(studentRepository.save(any(Student.class))).thenReturn(student);
+
+        JSONObject studentObject = new JSONObject();
+        studentObject.put("id", student.getId());
+        studentObject.put("name", student.getName());
+        studentObject.put("age", student.getAge());
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/student/create_with_random_faculty")
+                        .content(studentObject.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(student.getId()))
+                .andExpect(jsonPath("$.name").value(student.getName()))
+                .andExpect(jsonPath("$.age").value(student.getAge()));
+    }
+
+    @Test
+    void createTest() throws Exception {
+        when(studentRepository.save(any(Student.class))).thenReturn(student);
+        when(facultyRepository.findByNameIgnoreCase(anyString())).thenReturn(Optional.of(faculty));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/student/create")
+                        .param("name", student.getName())
+                        .param("age", String.valueOf(student.getAge()))
+                        .param("facultyName", faculty.getName())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(student.getId()))
+                .andExpect(jsonPath("$.name").value(student.getName()))
+                .andExpect(jsonPath("$.age").value(student.getAge()));
+    }
+
+    @Test
+    void getTest() throws Exception {
+        when(studentRepository.findById(anyLong())).thenReturn(Optional.of(student));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/student/{id}", student.getId())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(student.getId()))
+                .andExpect(jsonPath("$.name").value(student.getName()))
+                .andExpect(jsonPath("$.age").value(student.getAge()));
+
+        // тест получения несуществующего студента
+        when(studentRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/student/{id}", student.getId())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void editTest() throws Exception {
+        when(studentRepository.findById(anyLong())).thenReturn(Optional.of(student));
+        when(studentRepository.save(any(Student.class))).thenReturn(student);
+
+        String editedName = "Edited Name";
+        int editedAge = student.getAge() + 100;
+
+        JSONObject studentObject = new JSONObject();
+        studentObject.put("id", student.getId());
+        studentObject.put("name", editedName);
+        studentObject.put("age", editedAge);
+
+        mockMvc.perform((MockMvcRequestBuilders
+                        .put("/student"))
+                        .content(studentObject.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(student.getId()))
+                .andExpect(jsonPath("$.name").value(editedName))
+                .andExpect(jsonPath("$.age").value(editedAge));
+
+        // тест редактирования несуществующего студента
+        when(studentRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        mockMvc.perform((MockMvcRequestBuilders
+                        .put("/student"))
+                        .content(studentObject.toString())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
 }
